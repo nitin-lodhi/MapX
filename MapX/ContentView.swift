58c08385723df41a2af96ca52b45d72f5172bfaa
Nitin Lodhi <nitinlodhi101@gmail.com>
2024-07-07 00:37:25 +0530

    Initial Commit

---
+++ MapX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapX
+//
+//  Created by NITIN LODHI on 07/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapX/MapXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapXApp.swift
+//  MapX
+//
+//  Created by NITIN LODHI on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MapXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
